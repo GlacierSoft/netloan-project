8ee0968c82228b1a493c777a17a19ff77f87cd27
zhangzhenfei <289556866@qq.com>
2014-02-13 16:01:58 +0800

    前台主页增加bootstrap支持

---
+++ netloan-website/src/main/java/com/glacier/netloan/web/controller/common/CommonController.java
@@ -0,0 +1,50 @@
+/**
+ * @Title: CommonController.java 
+ * @Package com.glacier.netloan.web.controller.common 
+ * @author zhenfei.zhang   
+ * @email 289556866@qq.com
+ * @date 2014-2-13 下午3:29:07 
+ * @company (开发公司)    珠海市冰川软件有限公司
+ * @copyright (版权)    本文件归属珠海市冰川软件有限公司所有
+ * @version V1.0
+ * @modify (修改) 
+ *        	<p>
+				第一次修改：
+				时间：2014-2-13
+				修改人：zhenfei.zhang 
+				修改内容简介 ：
+			</p>              
+ * @Review (审核人) ：zhenfei.zhang
+ * 
+ */
+package com.glacier.netloan.web.controller.common;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * @ClassName: CommonController 
+ * @Description: TODO(公用控制器) 
+ * @author zhenfei.zhang
+ * @email 289556866@qq.com
+ * @date 2014-2-13 下午3:29:07
+ */
+@Controller
+public class CommonController {
+    
+    /**
+     * 
+     * @Title: index 
+     * @Description: TODO(默认访问控制器跳转至index.jsp) 
+     * @param  @return
+     * @throws 
+     * 备注<p>已检查测试:Green<p>
+     */
+    @RequestMapping(value = "/")
+    private Object index(){
+        ModelAndView mav = new ModelAndView("index");
+        return mav;
+    }
+
+}
---
+++ netloan-website/src/main/webapp/WEB-INF/views/inc.jsp
@@ -0,0 +1,14 @@
+<%@ page language="java" pageEncoding="UTF-8" contentType="text/html; charset=UTF-8"%>
+<!-- 引入jstl解析标签 -->
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+
+<!-- 引入网站标识icon -->
+<link rel="shortcut icon" href="${pageContext.request.contextPath}/resources/images/favicon/favicon.ico"></link>
+<link rel="icon" href="${pageContext.request.contextPath}/resources/images/favicon/favicon.ico"></link>
+
+<!-- 引入bootstrap样式 -->
+<link rel="stylesheet" href="${pageContext.request.contextPath}/resources/js/bootstrap-3.0.3-dist/dist/css/bootstrap.min.css"></link>
+
+<!-- 自定义主页样式 -->
+<link rel="stylesheet" href="${pageContext.request.contextPath}/resources/css/index.css"></link>
+<link rel="stylesheet" href="${pageContext.request.contextPath}/resources/css/ie8.css"></link>
