79a9d629e907a6aed3b8e4f3d02c01ee258032e5
dongxichao <406592176@qq.com>
2014-02-22 14:16:05 +0800

    重新删除会员生成类。

---
+++ netloan-webapp/src/main/java/com/glacier/netloan/web/controller/member/MemberIntegralController.java
@@ -0,0 +1,17 @@
+/*
+ * @(#)MemberIntegralController.java
+ * @author xichao.dong
+ * Copyright (c) 2013 Glacier SoftWare Company Limited. All Rights Reserved.
+ */
+package com.glacier.netloan.web.controller.member;
+
+/** 
+ * @ClassName: MemberIntegralController 
+ * @Description: TODO(这里用一句话描述这个类的作用) 
+ * @author xichao.dong
+ * @email 406592176@QQ.com
+ * @date 2014-2-22 下午1:46:53  
+ */
+public class MemberIntegralController {
+
+}
