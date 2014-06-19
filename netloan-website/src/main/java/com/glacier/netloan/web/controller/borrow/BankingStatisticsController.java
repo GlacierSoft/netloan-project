354f8e7f08e42678499c5a1f6ce742e342e1dc1f
zhangzhenfei <289556866@qq.com>
2014-06-19 15:37:35 +0800

    提交前台投资统计数据的完善。

--- netloan-website/src/main/java/com/glacier/netloan/web/controller/borrow/BankingStatisticsController.java
+++ netloan-website/src/main/java/com/glacier/netloan/web/controller/borrow/BankingStatisticsController.java
@@ -6,10 +6,11 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
-import com.alibaba.fastjson.JSONObject;
 import com.glacier.core.controller.AbstractController;
 import com.glacier.netloan.service.borrow.BankingStatisticsService;
+import com.glacier.netloan.service.member.MemberStatisticsService;
 
 /**
  * @ClassName: BankingStatisticsController 
@@ -24,14 +25,22 @@ public class BankingStatisticsController extends AbstractController{
 	
 	@Autowired
 	private BankingStatisticsService bankingStatisticsService;// 注入关注借款业务Bean
+	
+	@Autowired
+	private MemberStatisticsService memberStatisticsService;
 
 	// 进入个人理财统计展示页面
     @RequestMapping(value = "/memberBankingStatistics.htm")
     private Object memberBankingStatistics(String memberId,HttpServletRequest request) {
-        //查询个人理财统计
-    	JSONObject obj = (JSONObject)bankingStatisticsService.getBankingStatistics(memberId);
+        //查询个人理财统计,注释方法为泽徐所写
+/*    	JSONObject obj = (JSONObject)bankingStatisticsService.getBankingStatistics(memberId);
     	request.setAttribute("MemberBankingStatistics", obj);
-        return "member_mgr/memberBankingStatistics";
+        return "member_mgr/memberBankingStatistics";*/
+        
+        //新的统计方法，直接读取会员统计表
+        ModelAndView mav = new ModelAndView("member_mgr/memberBankingStatistics");
+        mav.addObject("memberStatisticsDate",memberStatisticsService.getStatisticsByMember(memberId));
+        return mav;
     }
     
 }
