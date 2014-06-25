e4153c3e7aea3b0264246d91ba8b98135b0837cc
zhangzhenfei <289556866@qq.com>
2014-06-25 15:47:37 +0800

    更新数据库，增加积分类别模块。

---
+++ netloan-module/src/main/java/com/glacier/netloan/entity/basicdatas/ParameterIntegralType.java
@@ -0,0 +1,134 @@
+package com.glacier.netloan.entity.basicdatas;
+
+import java.util.Date;
+
+public class ParameterIntegralType {
+    private String integralTypeId;
+
+    private String integralType;
+
+    private String changeType;
+
+    private Integer changeValue;
+
+    private String remark;
+
+    private String creater;
+
+    private Date createTime;
+
+    private String updater;
+
+    private Date updateTime;
+
+    public String getIntegralTypeId() {
+        return integralTypeId;
+    }
+
+    public void setIntegralTypeId(String integralTypeId) {
+        this.integralTypeId = integralTypeId;
+    }
+
+    public String getIntegralType() {
+        return integralType;
+    }
+
+    public void setIntegralType(String integralType) {
+        this.integralType = integralType;
+    }
+
+    public String getChangeType() {
+        return changeType;
+    }
+
+    public void setChangeType(String changeType) {
+        this.changeType = changeType;
+    }
+
+    public Integer getChangeValue() {
+        return changeValue;
+    }
+
+    public void setChangeValue(Integer changeValue) {
+        this.changeValue = changeValue;
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark;
+    }
+
+    public String getCreater() {
+        return creater;
+    }
+
+    public void setCreater(String creater) {
+        this.creater = creater;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getUpdater() {
+        return updater;
+    }
+
+    public void setUpdater(String updater) {
+        this.updater = updater;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    @Override
+    public boolean equals(Object that) {
+        if (this == that) {
+            return true;
+        }
+        if (that == null) {
+            return false;
+        }
+        if (getClass() != that.getClass()) {
+            return false;
+        }
+        ParameterIntegralType other = (ParameterIntegralType) that;
+        return (this.getIntegralTypeId() == null ? other.getIntegralTypeId() == null : this.getIntegralTypeId().equals(other.getIntegralTypeId()))
+            && (this.getIntegralType() == null ? other.getIntegralType() == null : this.getIntegralType().equals(other.getIntegralType()))
+            && (this.getChangeType() == null ? other.getChangeType() == null : this.getChangeType().equals(other.getChangeType()))
+            && (this.getChangeValue() == null ? other.getChangeValue() == null : this.getChangeValue().equals(other.getChangeValue()))
+            && (this.getRemark() == null ? other.getRemark() == null : this.getRemark().equals(other.getRemark()))
+            && (this.getCreater() == null ? other.getCreater() == null : this.getCreater().equals(other.getCreater()))
+            && (this.getCreateTime() == null ? other.getCreateTime() == null : this.getCreateTime().equals(other.getCreateTime()))
+            && (this.getUpdater() == null ? other.getUpdater() == null : this.getUpdater().equals(other.getUpdater()))
+            && (this.getUpdateTime() == null ? other.getUpdateTime() == null : this.getUpdateTime().equals(other.getUpdateTime()));
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((getIntegralTypeId() == null) ? 0 : getIntegralTypeId().hashCode());
+        result = prime * result + ((getIntegralType() == null) ? 0 : getIntegralType().hashCode());
+        result = prime * result + ((getChangeType() == null) ? 0 : getChangeType().hashCode());
+        result = prime * result + ((getChangeValue() == null) ? 0 : getChangeValue().hashCode());
+        result = prime * result + ((getRemark() == null) ? 0 : getRemark().hashCode());
+        result = prime * result + ((getCreater() == null) ? 0 : getCreater().hashCode());
+        result = prime * result + ((getCreateTime() == null) ? 0 : getCreateTime().hashCode());
+        result = prime * result + ((getUpdater() == null) ? 0 : getUpdater().hashCode());
+        result = prime * result + ((getUpdateTime() == null) ? 0 : getUpdateTime().hashCode());
+        return result;
+    }
+}
