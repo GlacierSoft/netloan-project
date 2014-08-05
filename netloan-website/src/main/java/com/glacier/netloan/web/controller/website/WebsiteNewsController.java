a5532e650438bdcd020ad7a5f706707454f66ace
songjundong <985776597@qq.com>
2014-08-05 15:55:51 +0800

    前台控制层优化

--- netloan-website/src/main/java/com/glacier/netloan/web/controller/website/WebsiteNewsController.java
+++ netloan-website/src/main/java/com/glacier/netloan/web/controller/website/WebsiteNewsController.java
@@ -3,16 +3,9 @@
  * @author xichao.dong
  * Copyright (c) 2013 Glacier SoftWare Company Limited. All Rights Reserved.
  */
-package com.glacier.netloan.web.controller.website;
-
-import java.util.List;
-
-import javax.validation.Valid;
-
-import org.apache.commons.lang3.StringUtils;
+package com.glacier.netloan.web.controller.website; 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
+import org.springframework.stereotype.Controller; 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,8 +13,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.glacier.core.controller.AbstractController;
-import com.glacier.jqueryui.util.JqPager;
-import com.glacier.netloan.entity.website.WebsiteNews;
+import com.glacier.jqueryui.util.JqPager; 
 import com.glacier.netloan.service.website.WebsiteNewsService;
 
 /** 
@@ -60,58 +52,12 @@ public class WebsiteNewsController extends AbstractController{
         ModelAndView mav = new ModelAndView("website_mgr/news_mgr/news");
         return mav;
     }
-    
-    // 进入新闻Form表单页面
-    @RequestMapping(value = "/intoForm.htm")
-    private Object intoNewsFormPnews(String webNewsId) {
-        ModelAndView mav = new ModelAndView("website_mgr/news_mgr/news_form");
-        if(StringUtils.isNotBlank(webNewsId)){
-            mav.addObject("newsData", newsService.getNews(webNewsId));
-        }
-        return mav;
-    }
-    
-    // 进入新闻Detail信息页面
-    @RequestMapping(value = "/intoDetail.htm")
-    private Object intoNewsDetailPage(String webNewsId) {
-        ModelAndView mav = new ModelAndView("website_mgr/news_mgr/news_detail");
-        if(StringUtils.isNotBlank(webNewsId)){
-            mav.addObject("newsData", newsService.getNews(webNewsId));
-        }
-        return mav;
-    }
+     
     
     // 获取表格结构的所有新闻数据
     @RequestMapping(value = "/list.json", method = RequestMethod.POST)
     @ResponseBody
     private Object listNewsAsGridByMenuId(JqPager pnewsr) {
         return newsService.listAsGrid(pnewsr);
-    }
-    
-    // 增加新闻
-    @RequestMapping(value = "/add.json", method = RequestMethod.POST)
-    @ResponseBody
-    private Object addNews(@Valid WebsiteNews news, BindingResult bindingResult) {
-        if (bindingResult.hasErrors()) {// 后台校验的错误信息
-            return returnErrorBindingResult(bindingResult);
-        }
-        return newsService.addNews(news);
-    }
-    
-    // 修改新闻
-    @RequestMapping(value = "/edit.json", method = RequestMethod.POST)
-    @ResponseBody
-    private Object editNews(@Valid WebsiteNews news, BindingResult bindingResult) {
-        if (bindingResult.hasErrors()) {// 后台校验的错误信息
-            return returnErrorBindingResult(bindingResult);
-        }
-        return newsService.editNews(news);
-    }
-    
-    // 批量删除新闻
-    @RequestMapping(value = "/del.json", method = RequestMethod.POST)
-    @ResponseBody
-    public Object delNews(@RequestParam List<String> webNewsIds,@RequestParam List<String> webNewsThemes) {
-    	return newsService.delNews(webNewsIds, webNewsThemes);
-    }
+    } 
 }
--- netloan-website/src/main/java/com/glacier/netloan/web/controller/website/WebsiteServiceController.java
+++ netloan-website/src/main/java/com/glacier/netloan/web/controller/website/WebsiteServiceController.java
@@ -3,25 +3,15 @@
  * @author xichao.dong
  * Copyright (c) 2013 Glacier SoftWare Company Limited. All Rights Reserved.
  */
-package com.glacier.netloan.web.controller.website;
-
-import java.util.List;
-
-import javax.validation.Valid;
-
-import org.apache.commons.lang3.StringUtils;
+package com.glacier.netloan.web.controller.website; 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
+import org.springframework.stereotype.Controller; 
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.RequestMethod; 
+import org.springframework.web.bind.annotation.ResponseBody; 
 
 import com.glacier.core.controller.AbstractController;
-import com.glacier.jqueryui.util.JqPager;
-import com.glacier.netloan.entity.website.WebsiteService;
+import com.glacier.jqueryui.util.JqPager; 
 import com.glacier.netloan.service.website.WebsiteServiceService;
 
 /**
@@ -37,65 +27,11 @@ public class WebsiteServiceController extends AbstractController{
 
     @Autowired
     private WebsiteServiceService websiteServiceService;// 注入客服业务Bean
-    
-    // 进入客服列表展示页面
-    @RequestMapping(value = "/index.htm")
-    private Object intoIndexPservice() {
-        ModelAndView mav = new ModelAndView("website_mgr/service_mgr/service");
-        return mav;
-    }
-    
-    // 进入客服Form表单页面
-    @RequestMapping(value = "/intoForm.htm")
-    private Object intoWebsiteServiceFormPservice(String webServiceId) {
-        ModelAndView mav = new ModelAndView("website_mgr/service_mgr/service_form");
-        if(StringUtils.isNotBlank(webServiceId)){
-            mav.addObject("serviceData", websiteServiceService.getWebsiteService(webServiceId));
-        }
-        return mav;
-    }
-    
-    // 进入客服Detail信息页面
-    @RequestMapping(value = "/intoDetail.htm")
-    private Object intoWebsiteServiceDetailPage(String webServiceId) {
-        ModelAndView mav = new ModelAndView("website_mgr/service_mgr/service_detail");
-        if(StringUtils.isNotBlank(webServiceId)){
-            mav.addObject("serviceData", websiteServiceService.getWebsiteService(webServiceId));
-        }
-        return mav;
-    }
-    
+     
     // 获取表格结构的所有菜单数据
     @RequestMapping(value = "/list.json", method = RequestMethod.POST)
     @ResponseBody
     private Object listActionAsGridByMenuId(JqPager pservicer) {
         return websiteServiceService.listAsGrid(pservicer);
-    }
-    
-    // 增加客服
-    @RequestMapping(value = "/add.json", method = RequestMethod.POST)
-    @ResponseBody
-    private Object addWebsiteService(@Valid WebsiteService service, BindingResult bindingResult) {
-        if (bindingResult.hasErrors()) {// 后台校验的错误信息
-            return returnErrorBindingResult(bindingResult);
-        }
-        return websiteServiceService.addWebsiteService(service);
-    }
-    
-    // 修改客服
-    @RequestMapping(value = "/edit.json", method = RequestMethod.POST)
-    @ResponseBody
-    private Object editWebsiteService(@Valid WebsiteService service, BindingResult bindingResult) {
-        if (bindingResult.hasErrors()) {// 后台校验的错误信息
-            return returnErrorBindingResult(bindingResult);
-        }
-        return websiteServiceService.editWebsiteService(service);
-    }
-    
-    // 批量删除客服
-    @RequestMapping(value = "/del.json", method = RequestMethod.POST)
-    @ResponseBody
-    public Object delWebsiteService(@RequestParam List<String> webServiceIds,@RequestParam List<String> webServiceNames) {
-    	return websiteServiceService.delWebsiteService(webServiceIds, webServiceNames);
-    }
+    } 
 }
