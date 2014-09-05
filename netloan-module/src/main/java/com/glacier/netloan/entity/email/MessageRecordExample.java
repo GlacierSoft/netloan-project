7c22d56a64dad03896715e1e634754587d924cc5
wt <Administrator@HP-201406121401>
2014-09-05 08:45:22 +0800

    提交活动邮件的代码规范二

--- netloan-module/src/main/java/com/glacier/netloan/entity/email/MessageRecordExample.java
+++ netloan-module/src/main/java/com/glacier/netloan/entity/email/MessageRecordExample.java
@@ -4,8 +4,6 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-import com.glacier.netloan.entity.finance.FinanceMemberExample.Criteria;
-
 public class MessageRecordExample {
     protected String orderByClause;
 
